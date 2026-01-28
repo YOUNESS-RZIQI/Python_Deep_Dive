6be29b52dd9203e0d4f8755e03f99032125e24be
Youness Rziqi <rziqiyouness@gmail.com>
2026-01-28 21:35:26 +0100

    Update

--- Python_05/ex2/nexus_pipeline.py
+++ Python_05/ex2/nexus_pipeline.py
@@ -1,181 +1,181 @@
-# 1- super()
-# 2- try/excpt
-# 3- list & dict  comprihensions
-# 4- ABC & abstractmethod
-# 5- Protocol for duck typing
-# 6- collections module is authorized
-# 7- Tye hints from typing module including Protocol
+# # 1- super()
+# # 2- try/excpt
+# # 3- list & dict  comprihensions
+# # 4- ABC & abstractmethod
+# # 5- Protocol for duck typing
+# # 6- collections module is authorized
+# # 7- Tye hints from typing module including Protocol
 
-from typing import Any, Dict, List, Protocol, Union
-from collections import deque
-from abc import ABC, abstractmethod
+# from typing import Any, Dict, List, Protocol, Union
+# from collections import deque
+# from abc import ABC, abstractmethod
 
-class ProcessingStage(Protocol):
-    """
-    Protocol class.
-    """
+# class ProcessingStage(Protocol):
+#     """
+#     Protocol class.
+#     """
 
-    def process(self, data: Any) -> Any:
-        """
-        Protocol method.
-        """
-        pass
-
-
-
-class ProcessingPipeline(ABC):
-    """
-    Abctract class Processing Pipeline.
-    """
-
-    def __init__(self) -> None:
-
-        """
-
-        """
-        self.stages: List[ProcessingStage] = []
-
-    def add_stage(self) -> None:
-        """
-
-        """
-        pass
-
-    @abstractmethod
-    def process(self, data: Any) -> Any:
-        """
-
-        """
-        return
+#     def process(self, data: Any) -> Any:
+#         """
+#         Protocol method.
+#         """
+#         pass
 
 
 
+# class ProcessingPipeline(ABC):
+#     """
+#     Abctract class Processing Pipeline.
+#     """
 
+#     def __init__(self) -> None:
 
+#         """
 
-class InputStage():
-    """
+#         """
+#         self.stages: List[ProcessingStage] = []
 
-    """
+#     def add_stage(self) -> None:
+#         """
 
-    def process(self, data: Any) -> Dict:
-        """
+#         """
+#         pass
 
-        """
-        return {}
+#     @abstractmethod
+#     def process(self, data: Any) -> Any:
+#         """
 
-
-class TransformStage():
-    """
-
-    """
-
-    def process(self, data: Any) -> Dict:
-        """
-
-        """
-        return {}
-
-
-class OutputStage():
-    """
-
-    """
-
-    def process(self, data: Any) -> str:
-        """
-
-        """
-        return ""
+#         """
+#         return
 
 
 
 
 
-class JSONAdapter(ProcessingPipeline):
 
-    """
+# class InputStage():
+#     """
+
+#     """
+
+#     def process(self, data: Any) -> Dict:
+#         """
+
+#         """
+#         return {}
+
+
+# class TransformStage():
+#     """
+
+#     """
+
+#     def process(self, data: Any) -> Dict:
+#         """
+
+#         """
+#         return {}
+
+
+# class OutputStage():
+#     """
+
+#     """
+
+#     def process(self, data: Any) -> str:
+#         """
+
+#         """
+#         return ""
+
+
+
+
+
+# class JSONAdapter(ProcessingPipeline):
+
+#     """
     
-    """
+#     """
 
-    def __init__(self) -> None:
-        """
+#     def __init__(self) -> None:
+#         """
 
-        """
-        pass
+#         """
+#         pass
 
-    def process(self, data: Any) -> Union[str,Any]:
-        """
+#     def process(self, data: Any) -> Union[str,Any]:
+#         """
 
-        """
-        return ""
+#         """
+#         return ""
 
 
-class CSVAdapter(ProcessingPipeline):
+# class CSVAdapter(ProcessingPipeline):
 
-    """
+#     """
     
-    """
+#     """
 
-    def __init__(self) -> None:
-        """
+#     def __init__(self) -> None:
+#         """
 
-        """
-        pass
+#         """
+#         pass
 
-    def process(self, data: Any) -> Union[str, Any]:
-        """
+#     def process(self, data: Any) -> Union[str, Any]:
+#         """
 
-        """
-        return ""
+#         """
+#         return ""
 
 
-class StreamAdapter(ProcessingPipeline):
+# class StreamAdapter(ProcessingPipeline):
 
-    """
+#     """
     
-    """
+#     """
 
-    def __init__(self) -> None:
-        """
+#     def __init__(self) -> None:
+#         """
 
-        """
-        pass
+#         """
+#         pass
 
-    def process(self, data: Any) -> Union[str, Any]:
-        """
+#     def process(self, data: Any) -> Union[str, Any]:
+#         """
 
-        """
-        return ""
+#         """
+#         return ""
 
 
-class NexusManager(ProcessingPipeline):
-    """
+# class NexusManager(ProcessingPipeline):
+#     """
 
-    """
+#     """
 
-    def __init__(self, pipeline_id: str) -> None:
+#     def __init__(self, pipeline_id: str) -> None:
         
-        """
+#         """
 
-        """
-        self.pipelines: List[ProcessingPipeline] = []
+#         """
+#         self.pipelines: List[ProcessingPipeline] = []
 
-    def add_pipeline(self, data: Any) -> None:
-        """
+#     def add_pipeline(self, data: Any) -> None:
+#         """
 
-        """
+#         """
 
-        pass
+#         pass
 
-    def process(self, data: Any) -> Union[str,Any]:
-        """
+#     def process(self, data: Any) -> Union[str,Any]:
+#         """
 
-        """
+#         """
 
-        return ""
+#         return ""
 
 
 
-# if __name__ == "__main__":
-#     main()
+# # if __name__ == "__main__":
+# #     main()
