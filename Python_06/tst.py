ebb8e21f35dbe46169af058d1155fdbd5ece3205
Youness Rziqi <rziqiyouness@gmail.com>
2026-02-01 21:04:25 +0100

    Update

--- Python_06/tst.py
+++ Python_06/tst.py
@@ -1,23 +1,23 @@
-# # ⚗️      ⚗️
-# #  ⚗️    ⚗️
-# #   ⚗️  ⚗️
-# #    ⚗️⚗️     ⚗️⚗️⚗️     ⚗️      ⚗️   ⚗️      ⚗️   ⚗️⚗️⚗️⚗️   ⚗️⚗️⚗️   ⚗️⚗️⚗️
-# #     ⚗️     ⚗️     ⚗️   ⚗️      ⚗️   ⚗️⚗️    ⚗️   ⚗️        ⚗️       ⚗️
-# #     ⚗️     ⚗️     ⚗️   ⚗️      ⚗️   ⚗️ ⚗️   ⚗️   ⚗️⚗️⚗️     ⚗️⚗️⚗️   ⚗️⚗️⚗️
-# #     ⚗️     ⚗️     ⚗️   ⚗️      ⚗️   ⚗️  ⚗️  ⚗️   ⚗️             ⚗️      ⚗️
-# #     ⚗️      ⚗️⚗️⚗️      ⚗️⚗️⚗️⚗️    ⚗️   ⚗️ ⚗️   ⚗️⚗️⚗️⚗️  ⚗️⚗️⚗️   ⚗️⚗️
-# #
-# #
-# #
-# # 1) check if alredy exist in modules ?
-# # 2) if not , check if exist in the builtin modules
-# # 3) load it .
-# # 4) excute it .
-# # 5) add it as loaded in the sys.madules.
-# def prnt():
-#     x = 10
-#     y = "hello"
-#     print(locals())
+# ⚗️      ⚗️
+#  ⚗️    ⚗️
+#   ⚗️  ⚗️
+#    ⚗️⚗️     ⚗️⚗️⚗️     ⚗️      ⚗️   ⚗️      ⚗️   ⚗️⚗️⚗️⚗️   ⚗️⚗️⚗️   ⚗️⚗️⚗️
+#     ⚗️     ⚗️     ⚗️   ⚗️      ⚗️   ⚗️⚗️    ⚗️   ⚗️        ⚗️       ⚗️
+#     ⚗️     ⚗️     ⚗️   ⚗️      ⚗️   ⚗️ ⚗️   ⚗️   ⚗️⚗️⚗️     ⚗️⚗️⚗️   ⚗️⚗️⚗️
+#     ⚗️     ⚗️     ⚗️   ⚗️      ⚗️   ⚗️  ⚗️  ⚗️   ⚗️             ⚗️      ⚗️
+#     ⚗️      ⚗️⚗️⚗️      ⚗️⚗️⚗️⚗️    ⚗️   ⚗️ ⚗️   ⚗️⚗️⚗️⚗️  ⚗️⚗️⚗️   ⚗️⚗️
+#
+#
+#
+# 1) check if alredy exist in modules ?
+# 2) if not , check if exist in the builtin modules
+# 3) load it .
+# 4) excute it .
+# 5) add it as loaded in the sys.madules.
+def prnt():
+    x = 10
+    y = "hello"
+    print(locals())
 
-# # The global namespace IS a dictionary:
-# prnt()
+# The global namespace IS a dictionary:
+prnt()
