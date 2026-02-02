a5cca91179a85b911431e4030f5d6dda39ed6d03
Youness Rziqi <rziqiyouness@gmail.com>
2026-02-02 15:27:59 +0100

    Update

---
+++ Python_06/alchemy/potions.py
@@ -0,0 +1,25 @@
+import alchemy
+from .elements import create_air, create_earth
+
+
+def healing_potion() -> str:
+    return (f"Healing potion brewed with "
+            f"{alchemy.elements.create_fire()} and "
+            f"{alchemy.elements.create_water()}")
+
+
+def strength_potion() -> str:
+    return (f"Strength potion brewed with "
+            f"{create_earth()} and {alchemy.elements.create_fire()}")
+
+
+def invisibility_potion() -> str:
+    return (f"Invisibility potion brewed with "
+            f"{create_air} and {alchemy.elements.create_water()}")
+
+
+def wisdom_potion() -> str:
+    return ("Wisdom potion brewed with all elements:" +
+            alchemy.elements.create_fire() +
+            alchemy.elements.create_water() +
+            create_earth() + create_air())
