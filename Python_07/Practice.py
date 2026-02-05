57c643257f78635e6081e0498e060ad509b015c7
Youness Rziqi <rziqiyouness@gmail.com>
2026-02-05 19:10:09 +0100

    Update

--- Python_07/Practice.py
+++ Python_07/Practice.py
@@ -1,8 +1,16 @@
 # import random
 
-# shuffle : changes in the original list, work only with the list type & it does return nothing.
+# # shuffle : changes in the original list, work only with the list type & it does return nothing.
+# deck = ["A♠", "2♠", "3♠", "4♠"]
+# result = random.shuffle(deck)
+# print(deck)
+# print(result)
+# print('\n')
+
+
+# choice : deal with the list, tuple and string (iterabel, indexed) , it return a random value from the iterable (list...).
 deck = ["A♠", "2♠", "3♠", "4♠"]
-result = random.shuffle(deck)
+result = random.choice(deck)
 print(deck)
 print(result)
 print('\n')
