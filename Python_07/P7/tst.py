fc6f34dbf44ec3c3491e454ec7db97de5ac02396
Youness Rziqi <rziqiyouness@gmail.com>
2026-02-13 12:13:04 +0100

    Updata

---
+++ Python_07/P7/tst.py
@@ -0,0 +1,20 @@
+# # Simple Card class for testing
+# class Card:
+#     def __init__(self, name: str, cost: int):
+#         self.name = name
+#         self.cost = cost
+
+
+# # Create some test cards
+# card1 = Card("Dragon", 5)
+# card2 = Card("Fireball", 3)
+# card3 = Card("Goblin", 1)
+
+# # Put them in a hand list
+# hand = [card1, card2, card3]
+
+# # Your join line
+# s = ", ".join(f"{card.name} ({card.cost})" for card in hand)
+
+# print("Joined string:", s)
+
